6e63fda14de6dcf945fadde02899e4e1b0682e5e
Rashko Mollov <rashko.1986@gmail.com>
2022-12-14 16:53:34 +0200

    Added Doctor Area

---
+++ Web/MCS.Web/Areas/Doctor/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿namespace MCS.Web.Areas.Doctor.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DashboardController : DoctorController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Web/MCS.Web/Areas/Doctor/Controllers/DoctorController.cs
@@ -0,0 +1,13 @@
+﻿namespace MCS.Web.Areas.Doctor.Controllers
+{
+    using MCS.Common;
+    using MCS.Web.Controllers;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.DoctorRoleName)]
+    [Area("Doctor")]
+    public class DoctorController : BaseController
+    {
+    }
+}
